e9ff7be1eb610ab56266684a8e25c133a21a82aa
DescendantOfdaVinci <128248529+DescendantOfdaVinci@users.noreply.github.com>
2023-11-28 01:16:18 +0500

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,8 +1,12 @@
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from hub.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('hub.urls'))
+    path('', EventListView.as_view(), name='eventlist'),
+    path('event/<slug:slug>/', EventDetailView.as_view(), name='eventdetail'),
+    path('create/', CreateEventView.as_view(), name='createevent'),
+    path('event/<int:pk>/delete/', DeleteEventView.as_view(), name='deleteevent'),
+    path('update/<slug:slug>/', UpdateEventView.as_view(), name='updateevent')
 ]
+
